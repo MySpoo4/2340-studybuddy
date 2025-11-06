d8192363d7b3126c16431d22868ecc5c0dfdc464
richrad <r@gmail.com>
2025-11-06 16:50:47 +0000

    pythonanywhere

--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-bilv=4o(xaq%+=q2n_zgnn7q51z(9-fp-j1wq@^-qwubrtxbgo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
